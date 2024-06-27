0da110f3a083dd2c5c8b85842d5163578429a2fe
AHMAD ABED <23041734@student.ltuc.com>
2024-06-27 18:42:45 +0300

    sad

---
+++ Console Challenges/Find Duplicates in Array/Find Duplicates in Array/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Find_Duplicates_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 3, 2, 1 };
+            int[] duplicatsArray = DuplicatesInArray(array);
+            foreach (var item in duplicatsArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static int[] DuplicatesInArray(int[] array)
+        {
+            List<int> duoblicateslist = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (IsduplicatsValue(array, array[i], i + 1))
+                {
+                    duoblicateslist.Add(array[i]);
+                }
+            }
+            return duoblicateslist.ToArray();
+
+        }
+        public static bool IsduplicatsValue(int[] array, int value, int index)
+        {
+            for (int i = index; i < array.Length; i++)
+            {
+                if (array[i] == value)
+
+                    return true;
+
+            }
+            return false;
+        }
+
+    }
+}
