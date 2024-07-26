61b840ad051a323ec1b7bb832eaacae0ef3b0d18
AHMAD ABED <23041734@student.ltuc.com>
2024-07-26 22:18:03 +0300

    stack&queue

---
+++ Data Structures/Stack & Queue/README.md
@@ -0,0 +1,10 @@
+#Problem Domain
+Implement basic stack and queue data structures to understand their functionality and usage
+
+##Whiteboard Images
+
+### Challenge Linked List Implementation
+![Remove Duplicates](./stack&queue.png)
+
+![](./Result.png)
+
---
+++ Data Structures/Stack & Queue/StackAndQueue1/StackAndQueue1/_Queue/Queue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue1._Queue
+{
+    public class Queue
+    {
+        Node front;
+        Node rear;
+
+        public Queue() 
+        {
+            front = null;
+            rear = null;
+        }
+
+        public void Enqueue(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (rear == null)
+            {
+                front = newNode;
+                rear = newNode;
+            }
+            else
+            {
+                rear.Next = newNode;
+                rear = newNode;
+            }
+
+           
+        }
+
+        public int Dequeue()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            int value = front.Data;
+            front = front.Next;
+
+            if (front == null)
+            {
+                rear = null;
+            }
+
+            return value;
+        }
+
+        public int Peek()
+        {
+            if (front == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return front.Data;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == null;
+        }
+        public void Print()
+        {
+            Node? current = front;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+
+    }
+}
