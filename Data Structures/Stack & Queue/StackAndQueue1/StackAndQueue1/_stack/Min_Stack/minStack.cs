5d4b110ebd93cac7a1cc86cc209926c30e9209c5
AHMAD ABED <23041734@student.ltuc.com>
2024-08-09 21:25:10 +0300

    add test

---
+++ Data Structures/Stack & Queue/StackAndQueue1/StackAndQueue1/_stack/Min_Stack/minStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue1._stack.Min_Stack
+{
+    public class minStack : Stack
+    {
+        public int GetMin()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("An error occurred.");
+            }
+            var ListElement = new List<int>();
+            Node node = top;
+            while (node != null)
+            {
+                ListElement.Add(node.Data);
+                node = node.Next;
+            }
+            return ListElement.Min();
+
+        }
+    }
+}
