f56e1839616a59f8590eabebc676288f6301442d
AHMAD ABED <23041734@student.ltuc.com>
2024-08-26 16:48:06 +0300

    Largest-Level-Value

--- Data Structures/Trees/TreeImplementation/TreeImplementation/Program.cs
+++ Data Structures/Trees/TreeImplementation/TreeImplementation/Program.cs
@@ -30,7 +30,7 @@ namespace TreeImplementation
             //bool contains = binarySearchTree.Contain(5); // contains: false
             //Console.WriteLine($"\nContains 5: {contains}");
 
-            Binary_Tree Btree = new Binary_Tree(4);
+            //Binary_Tree Btree = new Binary_Tree(4);
 
             //Btree.Root.left = new TNode(8);
             //Btree.Root.Right = new TNode(7);
@@ -63,24 +63,41 @@ namespace TreeImplementation
             //Console.WriteLine();
             //Console.WriteLine(Btree.SecondMax(Btree.Root));
 
-            Btree.Root = new TNode(10);
-            Btree.Root.left = new TNode(8);
-            Btree.Root.Right = new TNode(12);
-            Btree.Root.left.left = new TNode(4);
+            //Btree.Root = new TNode(10);
+            //Btree.Root.left = new TNode(8);
+            //Btree.Root.Right = new TNode(12);
+            //Btree.Root.left.left = new TNode(4);
+            //Btree.Root.left.Right = new TNode(7);
+            //Btree.Root.Right.left = new TNode(15);
+            //Btree.Root.Right.Right = new TNode(23);
+            //Btree.Root.left.left.Right = new TNode(3);
+
+
+            //Console.WriteLine("Pre-Order Traversal:");
+            //Btree.Pre_Order(Btree.Root);
+            //Console.WriteLine();
+
+
+            //int leafSum = Btree.LeafSum();
+            //Console.WriteLine("The sum of all leaf nodes is:");
+            //Console.WriteLine(leafSum);
+
+            Binary_Tree Btree = new Binary_Tree(5);
+
+            Btree.Root.left = new TNode(13);
+            Btree.Root.Right = new TNode(7);
+            Btree.Root.left.left = new TNode(3);
             Btree.Root.left.Right = new TNode(7);
-            Btree.Root.Right.left = new TNode(15);
-            Btree.Root.Right.Right = new TNode(23);
-            Btree.Root.left.left.Right = new TNode(3);
+            Btree.Root.Right.left = new TNode(12);
+            Btree.Root.Right.Right = new TNode(20);
+            Btree.Root.left.left.left = new TNode(1);
+            Btree.Root.left.left.Right = new TNode(4);
+            Btree.Root.Right.left.Right = new TNode(11);
 
+            List<int> largestValues = Btree.LargestValueEachLevel();
 
-            Console.WriteLine("Pre-Order Traversal:");
-            Btree.Pre_Order(Btree.Root);
-            Console.WriteLine();
-
-
-            int leafSum = Btree.LeafSum();
-            Console.WriteLine("The sum of all leaf nodes is:");
-            Console.WriteLine(leafSum);
+            // Print the largest values at each level
+            Console.WriteLine("Largest values at each level: " + string.Join(", ", largestValues));
 
 
         }
